26039e2a2e71952ab388faddedbd264182b571a8
Gabriel M. Alves <gabriel_cm15@hotmail.com>
2024-09-09 21:35:31 -0300

    eventos

--- my-app2/src/index.js
+++ my-app2/src/index.js
@@ -1,59 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Opa from './Opa'
 import reportWebVitals from './reportWebVitals';
+// import Lista from './Lista'
+// import Perfil from './Perfil'
+// import Botao from './Button';
+import ButtonComponent from './ButtonComponent'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-function Ola(props) {
-  return <h1>Olá {props.nome}</h1>
-}
-
-function Mensagem() {
-  return <ol>
-    <li>cidades</li>
-    <li>lugares</li>
-    <li>objetos</li>
-    <li>viagens</li>
-    <li>ferias</li>
-  </ol>
-}
-
-class Nome extends React.Component {
-  render() {
-    return <h2>Nome: {this.props.identificacao}</h2>
-  }
-}
-
-class Email extends React.Component {
-  render() {
-    return <h2>Email: {this.props.identificacao}</h2>
-  }
-}
-
-class Turma extends React.Component {
-  render() {
-    return <h2>Turma: {this.props.identificacao}</h2>
-  }
-}
-
-
-
 root.render(
   <div>
-    <Ola nome="Gabriel"></Ola>  
-    <Nome identificacao="Gabriel Menezes Alves"></Nome>
-    <Email identificacao="gabriel@123.com"></Email>
-    <Turma identificacao="13184"></Turma>
+    {/* <Lista titulo="Lista de itens"></Lista>
+    <Perfil perfil="Informações de perfil" nome="Valdair" area="Construção" hobbies="Pescaria"></Perfil> */}
 
-
-    <Mensagem></Mensagem>
+    {/* <Botao label="Click aqui!"></Botao> */}
+    <ButtonComponent label="Click aqui!"></ButtonComponent>
   </div>
 )
 
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
