9a51b4c24a826edf4b568bfebf099d324386f125
Gabriel M. Alves <gabriel_cm15@hotmail.com>
2024-09-16 21:57:04 -0300

    produtos1609

--- my-app2/src/index.js
+++ my-app2/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Atividade from './Atividade'
+//import Atividade from './Atividade'
+import Produto from './Produto';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <div>
-    <Atividade></Atividade>
+    <Produto></Produto>
   </div>
 )
 
