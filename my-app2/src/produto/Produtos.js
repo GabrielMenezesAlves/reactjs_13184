912731dd2ffc67107788a4e5cf704b1ad8e7b9c4
Gabriel M. Alves <gabriel_cm15@hotmail.com>
2024-09-23 22:00:02 -0300

    css produtos

--- my-app2/src/produto/Produtos.js
+++ my-app2/src/produto/Produtos.js
@@ -1,12 +1,15 @@
 
 import React, { useState, useEffect } from "react";
-import Modal from './modal/Modal'
+import './Produtos.css'
+
+import Modal from '../modal/Modal'
 
 function Produtos() {
     const [produto, setProduto] = useState({ nome: "", preco: "", descricao: "" })
     const [produtos, setProdutos] = useState([])
     const [modalVisivel, setModalVisivel] = useState(false)
     const [index, setIndex] = useState(undefined)
+    const [mensagem, setMensagem] = useState(undefined)
 
     useEffect(() => {
         const produtosLocalStorage = JSON.parse(localStorage.getItem('produtos'));
@@ -30,8 +33,9 @@ function Produtos() {
         setProdutos([...produtos, produto])
     }
 
-    const excluir = (index) => {
+    const excluir = (index, nomeProduto) => {
         setIndex(index)
+        setMensagem("Deseja realmente excluir o produto (" + nomeProduto + ")")
         setModalVisivel(true)
     }
 
@@ -47,7 +51,7 @@ function Produtos() {
     }
 
     return (
-        <>
+        <div className="container">
             <input
                 type="text"
                 name="nome"
@@ -83,7 +87,7 @@ function Produtos() {
                             <strong>Preço:</strong> {item.preco}<br />
                             <strong>Descricao:</strong> {item.descricao}
                         </p>
-                        <button onClick={() => excluir(index)}>Excluir</button>
+                        <button onClick={() => excluir(index, item.nome)}>Excluir</button>
 
                     </li>
                 ))}
@@ -92,10 +96,10 @@ function Produtos() {
             <Modal
                 ehVisivel={modalVisivel}
                 aoConfirmar={aoConfirmar}
-                aoCancelar={aoCancelar}>
+                aoCancelar={aoCancelar}
+                mensagem={mensagem}>
             </Modal>
-
-        </>
+        </div>
     )
 }
 
